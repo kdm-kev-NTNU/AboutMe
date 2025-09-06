29c89f571b74e99cc49c202958305db891492ad1
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-09-06 15:26:03 +0200

    config for domain name

--- backend/src/main/java/com/kevinmazali/portfolio/config/WebConfig.java
+++ backend/src/main/java/com/kevinmazali/portfolio/config/WebConfig.java
@@ -16,7 +16,9 @@ public class WebConfig {
                 registry.addMapping("/**")
                     .allowedOrigins(
                         "http://localhost:5173",
-                        "http://localhost:4173"
+                        "http://localhost:4173",
+                        "https://kevindmazali.me",
+                        "https://www.kevindmazali.me"
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
